2ae27d43502fddc43e894e698618207becc8fe14
liaowang <1245208481@qq.com>
2020-03-03 10:56:25 +0800

    完成了登录功能

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message, Icon } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Icon)
+Vue.prototype.$message = Message
